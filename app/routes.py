8db3c70d853e0d638cf1bca1a44b54a72ece96f1
rianreiss <rianreis2109@gmail.com>
2024-03-25 19:10:11 -0300

    routes and views updates

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/relatorio')
+def relatorio():
+    return render_template('relatorios.html')
